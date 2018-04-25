cf0917ab09bcc66b9b6fb4272b79c7ef8239eff9
cocorico35410 <alain.collet2@neuf.fr>
2018-04-25 00:58:03 -0400

    Test on loops of (describe/it closures) in protractor

---
+++ scenarios.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const _ = require('lodash');
+
+// Angular E2E Testing Guide:
+// https://docs.angularjs.org/guide/e2e-testing
+
+
+  const templates = [
+    {id: 0, template: 'template_1.xml'},
+    {id: 2, template: 'template_2.xml'},
+    {id: 4, template: 'template_3.xml'},
+    {id: 6, template: 'template_4.xml'}
+  ];
+
+
+
+function t_10(template) {
+
+  describe('t_10', () => {
+    it('test_10', done => {
+      browser.sleep(1000)
+      //Promise.resolve()
+        .then(() => console.log('test_10', template))
+        .then(done);
+    });
+  });
+
+
+}
+
+describe('My app', function() {
+
+  beforeEach(function() {
+    browser.ignoreSynchronization=true;
+    //browser.get('');
+  });
+
+
+  var funcs = [];
+  for (var i = 0; i < 3; i++) {      // let's create 3 functions
+    funcs[i] = function() {          // and store them in funcs
+      console.log("My value: " + i); // each should log its value.
+    };
+  }
+  for (var j = 0; j < 3; j++) {
+    funcs[j]();                      // and now let's run each one to see
+  }
+
+  //_.each(templates, template_e => {
+  _.each(templates, (t, i) => {
+  //for(var i=0; i<templates.length; i++) {
+    
+    var template_e = templates[i];
+    console.log('t', template_e);
+
+  //  ((template) => {
+
+      describe(`describe ${template_e.id}`, () => {
+
+        console.log('describe', template_e);
+
+        it('test_0', () => {
+          console.log('coucou');
+          browser.sleep(1000)
+          //Promise.resolve()
+            .then(() => console.log('test_0', template_e))
+            //.then(done);
+        });
+
+        it('test_1', done => {
+          console.log('test_1');
+          browser.sleep(1000)
+          //Promise.resolve()
+            .then(() => console.log('test_1', template_e))
+            .then(() => done());
+        });
+
+        t_10(template_e);
+
+      });
+
+//   })(template_e)
+  //};
+  });
+});
